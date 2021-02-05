1047b2a555a75a9c5296bddb52867221fbaaf0f1
aesop <ee.changsub@gmail.com>
2021-02-05 19:21:25 +0900

    Edit index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('hello practice node server-deploy')
-})
+app.get("/", (req, res) => {
+  console.log("index.js에 접근 중");
+  res.send("hello practice");
+});
 
-app.listen(5000, ()=> {
-  console.log('server on 5000')
-})
+// app.listen(5000, ()=> {
+//   console.log('server on 5000')
+// })
