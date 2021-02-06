d22c53201797fffe5a4c19e014ea90594586a015
aesop <ee.changsub@gmail.com>
2021-02-07 01:48:55 +0900

    Edit db-connecting part

--- db/index.js
+++ db/index.js
@@ -1,20 +1,22 @@
-const mysql = require("mysql");
+const mysql = require("mysql2");
 require("dotenv").config();
 
-const [name, username, password, host] = [
+const [name, username, password, host, port] = [
   process.env.DATABASE_NAME,
   process.env.DATABASE_USERNAME,
   process.env.DATABASE_PASSWORD,
   process.env.DATABASE_HOST,
+  process.env.DATABASE_PORT,
 ];
 
-const connection = mysql.createConnection({
+const db = mysql.createConnection({
   host: host,
+  port: port,
   user: username,
   password: password,
   database: name,
 });
 
-connection.connect();
+db.connect();
 
-module.exports = connection;
+module.exports = db;
